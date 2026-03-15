5afb68d5019aa978867e7a6420c0b652ccec7098
hungg261 <phanthanhhung.26012010@gmail.com>
2026-03-15 10:39:04 +0700

    set gen.sh to example

--- template/scripts/gen/gen.sh
+++ template/scripts/gen/gen.sh
@@ -3,34 +3,10 @@
 cd ../../bin
 mkdir -p ../tests
 
-for i in {1..10}
+for i in {1..40}
 do
     echo "Generating test $i"
-    ./gen $i small > ../tests/$i.inp
-done
-
-for i in {11..20}
-do
-    echo "Generating test $i"
-    ./gen $i random > ../tests/$i.inp
-done
-
-for i in {21..30}
-do
-    echo "Generating test $i"
-    ./gen $i t=1 > ../tests/$i.inp
-done
-
-for i in {31..31}
-do
-    echo "Generating test $i"
-    ./gen $i max-t=1 > ../tests/$i.inp
-done
-
-for i in {32..40}
-do
-    echo "Generating test $i"
-    ./gen $i max > ../tests/$i.inp
+    ./gen $i > ../tests/$i.inp
 done
 
 echo
