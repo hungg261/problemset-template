b8c1102a25b37b55fcddcd262c3c4a1bc2cce851
hungg261 <phanthanhhung.26012010@gmail.com>
2026-03-15 10:39:58 +0700

    skip checker if not exist

--- template/scripts/build/build_checker.sh
+++ template/scripts/build/build_checker.sh
@@ -1,4 +1,8 @@
 cd ../..
 
-echo "Compiling checker..."
-g++ checkers/checker.cpp -I. -O2 -o bin/checker
+if [ -f "checkers/checker.cpp" ]; then
+    echo "Compiling checker..."
+    g++ checkers/checker.cpp -I. -O2 -o bin/checker
+else
+    echo "Checker source not found, skipping."
+fi
