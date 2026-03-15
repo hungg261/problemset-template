99ac2147b6f62482ab56eb9cc945195a62c4701d
hungg261 <phanthanhhung.26012010@gmail.com>
2026-03-15 10:37:10 +0700

    set validator to example

--- template/validators/validator.cpp
+++ template/validators/validator.cpp
@@ -3,25 +3,9 @@
 using namespace std;
 
 signed main(int argc, char* argv[]){
-    ios_base::sync_with_stdio(0); cin.tie(0);
     registerValidation(argc, argv);
     
-    int t = inf.readInt(1, 1'000, "t");
-    inf.readEoln();
-
-    int sum_n = 0;
-    while(t--){
-        int n = inf.readInt(1, 1'000'000, "n");
-        inf.readSpace();
-        int k = inf.readInt(1, 26, "k");
-        inf.readEoln();
-
-        sum_n += n;
-        ensure(sum_n <= 1'000'000);
-    }
-
-    inf.skipBlanks();
-    inf.readEof();
+    // validate your test cases here.
 
     return 0;
 }
